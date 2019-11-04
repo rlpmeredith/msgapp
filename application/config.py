d5870561e6bba5990fe8552f28fbb99c3e19fe0b
roblpm <roblpm@hotmail.com>
2019-11-04 14:46:15 +0800

    Monday2

--- application/config.py
+++ application/config.py
@@ -3,5 +3,5 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
 	SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
-	SQLALCHEMY_DATABASE_URI = 'mysql://rob:harry5@127.0.0.1/msgdb'
+	SQLALCHEMY_DATABASE_URI = 'mysql://rob:$DB_PASSWORD@127.0.0.1/msgdb'
 	SQLALCHEMY_TRACK_MODIFICATIONS = False
